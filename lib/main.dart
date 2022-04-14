d67820afabe40e614683825ef7bd5e688af023f5
Jeremiah Jacob <jaykon4me@gmail.com>
2022-04-14 09:29:57 +0100

    complete base UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
+import 'package:chat_lit/theme.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'screens/home_screen.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat Lit',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: const Text('Hello World'),
-          ),
-        ),
-      ),
+      theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
+      themeMode: ThemeMode.dark,
+      title: 'Chatter',
+      home: HomeScreen(),
     );
   }
 }
